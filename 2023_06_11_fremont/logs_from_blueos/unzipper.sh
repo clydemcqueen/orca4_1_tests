f6a35277e8bbdf63b6dd85138323f3cc0de986d7
Clyde McQueen <clyde@mcqueen.net>
2023-07-08 08:51:24 -0700

    First commit

---
+++ 2023_06_11_fremont/logs_from_blueos/unzipper.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-144612.zip -d ardupilot-manager-20230611-144612
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-171020.zip -d ardupilot-manager-20230611-171020
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-170002.zip -d ardupilot-manager-20230611-170002
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-183011.zip -d ardupilot-manager-20230611-183011
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-164950.zip -d ardupilot-manager-20230611-164950
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-171951.zip -d ardupilot-manager-20230611-171951
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-163938.zip -d ardupilot-manager-20230611-163938
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-200636.zip -d ardupilot-manager-20230611-200636
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-143721.zip -d ardupilot-manager-20230611-143721
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-184025.zip -d ardupilot-manager-20230611-184025
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-152747.zip -d ardupilot-manager-20230611-152747
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-180929.zip -d ardupilot-manager-20230611-180929
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230610-192512.zip -d ardupilot-manager-20230610-192512
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-162924.zip -d ardupilot-manager-20230611-162924
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-202706.zip -d ardupilot-manager-20230611-202706
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230610-193552.zip -d ardupilot-manager-20230610-193552
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230610-195325.zip -d ardupilot-manager-20230610-195325
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-142650.zip -d ardupilot-manager-20230611-142650
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-161904.zip -d ardupilot-manager-20230611-161904
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-153810.zip -d ardupilot-manager-20230611-153810
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-181957.zip -d ardupilot-manager-20230611-181957
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-203719.zip -d ardupilot-manager-20230611-203719
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-154823.zip -d ardupilot-manager-20230611-154823
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-160849.zip -d ardupilot-manager-20230611-160849
+unzip /home/clyde/Desktop/orca4_1_tests/2023_06_11_fremont/logs_from_blueos/ardupilot-manager-20230611-155836.zip -d ardupilot-manager-20230611-155836
