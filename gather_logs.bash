c410399bb6d6ddbdff5244cf9c434465b08ab0fc
Clyde McQueen <clyde@mcqueen.net>
2023-07-13 11:47:12 -0700

    Start a script

---
+++ gather_logs.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Gather log files after a dive
+
+# TODO take a dir name, if it doesn't exist create it
+# TODO BIN files?
+
+mv /home/clyde/Documents/PingViewer/Sensor_Log/* .
+mv /home/clyde/Documents/QGroundControl/Telemetry/* .
